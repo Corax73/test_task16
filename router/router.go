507d45a9e06cf96056c35742fddb203f1c5ad2e3
Corax73 <ramaz999@inbox.ru>
2024-07-05 00:28:48 +0300

    work with start and stop tasks

--- router/router.go
+++ router/router.go
@@ -1,19 +1,26 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 	"timeTracker/customDb"
 	"timeTracker/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
-const noRecords = "try later"
+const somethingWrong = "try later"
+const noRecords = "not found"
 
 func RunRouter() {
 	router := gin.Default()
 	router.GET("/users", getUsers)
 	router.GET("/tasks", getTasks)
+	router.POST("/tasks/start", startTask)
+	router.POST("/tasks/stop", stopTask)
 	router.Run(":4343")
 }
 
@@ -24,7 +31,7 @@ func getUsers(c *gin.Context) {
 	if len(users) > 0 {
 		c.JSON(200, users)
 	} else {
-		c.JSON(http.StatusBadRequest, gin.H{"error": noRecords})
+		c.JSON(http.StatusBadRequest, gin.H{"error": somethingWrong})
 	}
 }
 
@@ -35,6 +42,60 @@ func getTasks(c *gin.Context) {
 	if len(tasks) > 0 {
 		c.JSON(200, tasks)
 	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": somethingWrong})
+	}
+}
+
+func startTask(c *gin.Context) {
+	paramId := c.DefaultPostForm("id", "0")
+	if paramId == "0" {
 		c.JSON(http.StatusBadRequest, gin.H{"error": noRecords})
+	} else {
+		database := customDb.GetConnect()
+		var count int64
+		database.Model(&models.Task{}).Where("id = ?", paramId).Count(&count)
+		if count > 0 {
+			taskId, err := uuid.Parse(fmt.Sprint(paramId))
+			if err == nil {
+				database.Model(&models.TaskExecutionTime{}).Where("task_id = ? AND pause IS NULL", taskId).Count(&count)
+				if count == 0 {
+					database.Save(&models.TaskExecutionTime{ID: uuid.New(), TaskId: taskId, StartExec: time.Now()})
+					c.JSON(200, "started")
+				} else if count > 0 {
+					c.JSON(http.StatusBadRequest, gin.H{"error": "already started"})
+				}
+			} else {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			}
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": noRecords})
+		}
+	}
+}
+
+func stopTask(c *gin.Context) {
+	paramId := c.DefaultPostForm("id", "0")
+	if paramId == "0" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": noRecords})
+	} else {
+		database := customDb.GetConnect()
+		var count int64
+		database.Model(&models.Task{}).Where("id = ?", paramId).Count(&count)
+		if count > 0 {
+			taskId, err := uuid.Parse(fmt.Sprint(paramId))
+			if err == nil {
+				database.Model(&models.TaskExecutionTime{}).Where("task_id = ? AND pause IS NULL", taskId).Count(&count)
+				if count == 1 {
+					result := database.Model(&models.TaskExecutionTime{}).Where("task_id = ? AND pause IS NULL", taskId).Update("pause", time.Now())
+					c.JSON(200, "updated "+strconv.FormatInt(result.RowsAffected, 10))
+				} else if count == 0 {
+					c.JSON(http.StatusBadRequest, gin.H{"error": "already stopped"})
+				}
+			} else {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			}
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": noRecords})
+		}
 	}
 }
